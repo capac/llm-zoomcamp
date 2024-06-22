f57ae964586e4ca835fd44b2dd16b0050ee81776
capac <541813+capac@users.noreply.github.com>
2024-06-22 19:30:18 +0000

    Code for homework #1

---
+++ homework/01-intro/homework_01.py
@@ -0,0 +1,75 @@
+import requests
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+docs_url = 'https://github.com/DataTalksClub/llm-zoomcamp/blob/main/01-intro/documents.json?raw=1'
+docs_response = requests.get(docs_url)
+documents_raw = docs_response.json()
+
+documents = []
+
+for course in documents_raw:
+    course_name = course['course']
+
+    for doc in course['documents']:
+        doc['course'] = course_name
+        documents.append(doc)
+
+es_client = Elasticsearch('http://localhost:9200')
+
+index_settings = {
+    'settings': {
+        'number_of_shards': 1,
+        'number_of_replicas': 0,
+    },
+    'mappings': {
+        'properties': {
+            'text': {'type': 'text'},
+            'selection': {'type': 'text'},
+            'question': {'type': 'text'},
+            'course': {'type': 'keyword'},
+        },
+    },
+}
+
+index_name = 'course-questions'
+query = 'How do I execute a command in a running docker container?'
+
+# check if the index exists
+if not es_client.indices.exists(index=index_name):
+    # create the index if it does not exist
+    try:
+        es_client.indices.create(index=index_name, body=index_settings)
+        print(f"Index '{index_name}' created successfully.")
+    except ElasticsearchException as e:
+        print(f"Error creating index: {e}")
+
+for doc in documents:
+    es_client.index(index=index_name, document=doc)
+
+search_query = {
+    'size': 5,
+    'query': {
+        'bool': {
+            'must': {
+                'multi_match': {
+                    'query': query,
+                    'fields': ['question^4', 'text'],
+                    'type': 'best_fields',
+                },
+            },
+            'filter': {
+                'term': {
+                    'course': 'machine-learning-zoomcamp',
+                },
+            },
+        },
+    },
+}
+
+response = es_client.search(index=index_name, body=search_query)
+results_docs = []
+for hit in response['hits']['hits']:
+    results_docs.append(hit)
+
+pprint(results_docs)
