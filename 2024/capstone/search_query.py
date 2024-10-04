14e14592cdf65b7933a5d9c15e2e0054efcc79e8
capac <capac@users.noreply.github.com>
2024-10-04 15:39:56 +0100

    Testing RAG with prompt template

--- 2024/capstone/search_query.py
+++ 2024/capstone/search_query.py
@@ -1,4 +1,8 @@
 from elasticsearch import Elasticsearch
+from openai import OpenAI
+
+# Initialize OpenAI client
+client = OpenAI()
 
 # Initialize Elasticsearch
 es = Elasticsearch("http://localhost:9200")
@@ -8,7 +12,7 @@ index_name = "solar_eclipse_chunks"
 
 
 # Query function to search for relevant answers
-def query_solar_eclipse(query):
+def query_database(query):
     # Construct the search query
     search_query = {
         "size": 5,  # Return the top 5 matches
@@ -19,7 +23,7 @@ def query_solar_eclipse(query):
                         "multi_match": {
                             "query": query,
                             # Prioritize text (boosted by ^3) over tables
-                            "fields": ["text^3", "table^1"],
+                            "fields": ["text^2", "table^1"],
                             "type": "best_fields"
                         }
                     }
@@ -58,26 +62,49 @@ def query_solar_eclipse(query):
     return results
 
 
+def build_prompt(query, search_results):
+    prompt_template = """
+You're an astronomor enthusiast. Answer the QUESTION based on the CONTEXT from
+the database. Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT: {context}
+""".strip()
+
+    context = ""
+
+    for doc in search_results:
+        context = context + f"text: {doc['text']}\ntable: {doc['table']}\n\n"
+
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+
+def llm(prompt):
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return response.choices[0].message.content
+
+
+def rag(query):
+    search_results = query_database(query)
+    prompt = build_prompt(query, search_results)
+    answer = llm(prompt)
+    return answer
+
+
 # Example: Query the index
 def main():
     # Example question
     user_query = "When will the next total solar eclipse occur in Europe?"
 
     # Query Elasticsearch for the answer
-    search_results = query_solar_eclipse(user_query)
-
-    # Print results
-    for result in search_results:
-        if result['text']:
-            print(f"Document ID: {result['doc_id']}")
-            print(f"Chunk ID: {result['chunk_id']}")
-            print(f"Text: {result['text']}")
-        elif result['table']:
-            print(f"Document ID: {result['doc_id']}")
-            print(f"Chunk ID: {result['chunk_id']}")
-            print("Table Data:")
-            for row in result['table']:
-                print(row)
+    search_results = rag(user_query)
+    print(search_results)
 
 
 if __name__ == "__main__":
