731b0c28db3ce53b966cee35cd2af7c267dc9a45
capac <capac@users.noreply.github.com>
2026-06-24 15:22:51 +0100

    Corrected OpenAI API base URL, and using correct OpenAI API key

--- 2026/check_openai_setup.py
+++ 2026/check_openai_setup.py
@@ -1,16 +1,19 @@
+import os
 from dotenv import load_dotenv
 from openai import OpenAI
-import os
 
 load_dotenv()
 
 openai_client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
-    base_url="https://api.openai.com/openai/v1"
+    base_url="https://api.openai.com/v1"
 )
 
 try:
-    openai_client.responses
-    print("OpenAI setup check completed.")
+    response = openai_client.responses.create(
+        model="gpt-5-mini",
+        input="What is the weather like today in London, UK?"
+        )
+    print(response.output_text)
 except Exception as e:
     print(f"Error occurred while setting up OpenAI: {e}")
