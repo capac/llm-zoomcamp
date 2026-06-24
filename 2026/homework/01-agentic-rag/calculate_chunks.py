1158b64a61b164839fbf5400178e1e1f868ab035
capac <capac@users.noreply.github.com>
2026-06-24 16:44:43 +0100

    Solution to homework question #5

--- 2026/homework/01-agentic-rag/calculate_chunks.py
+++ 2026/homework/01-agentic-rag/calculate_chunks.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 from gitsource import GithubRepositoryDataReader, chunk_documents
 from openai import OpenAI
+from minsearch import Index
+from rag_helper import RAGBase, INSTRUCTIONS, PROMPT_TEMPLATE
+
 
 load_dotenv()
 
@@ -30,3 +33,24 @@ for file in files:
 chunks = chunk_documents(documents, size=2000, step=1000)
 
 print(f"Number of chunks created: {len(chunks)}")
+
+index = Index(
+    text_fields=["content"],
+    keyword_fields=["filename"]
+)
+
+index.fit(chunks)
+print("Indexing completed.")
+
+question = "How does the agentic loop keep calling the model until it stops?"
+
+rag = RAGBase(
+    index=index,
+    llm_client=openai_client,
+    instructions=INSTRUCTIONS,
+    prompt_template=PROMPT_TEMPLATE,
+)
+
+print(f"Question: {question}")
+assistant = rag.rag_query(question)
+print(f"Number of input tokens: {assistant.usage.input_tokens}")
