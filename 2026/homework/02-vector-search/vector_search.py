cbb85ac7285d31c71a0ed659150996262824af54
capac <capac@users.noreply.github.com>
2026-07-02 09:54:25 +0100

    Created 'v_search' module
    Imported in 'hybrid_search' for homework #2 question #6

--- 2026/homework/02-vector-search/vector_search.py
+++ 2026/homework/02-vector-search/vector_search.py
@@ -2,26 +2,35 @@ from ingest import download_documents
 from embedder import Embedder
 from gitsource import chunk_documents
 from minsearch import VectorSearch
+from typing import List
 import numpy as np
 
-documents = download_documents()
-chunks = chunk_documents(documents, size=2000, step=1000)
 
-model = Embedder()
-chunks_embeddings = []
-for chunk in chunks:
-    embedding = model.encode(chunk["content"])
-    chunks_embeddings.append(embedding)
+def v_search(documents: List, question: str) -> List[str]:
+    chunks = chunk_documents(documents, size=2000, step=1000)
 
-X = np.array(chunks_embeddings)
+    model = Embedder()
+    chunks_embeddings = []
+    for chunk in chunks:
+        embedding = model.encode(chunk["content"])
+        chunks_embeddings.append(embedding)
 
-vector_index = VectorSearch(keyword_fields=["content"])
-vector_index.fit(X, chunks)
+    X = np.array(chunks_embeddings)
 
-# q1 = "What metric do we use to evaluate a search engine?"
-q1 = "How do I store vectors in PostgreSQL?"
-v1 = model.encode(q1)
+    vector_index = VectorSearch(keyword_fields=["content"])
+    vector_index.fit(X, chunks)
 
-results = vector_index.search(v1, num_results=5)
-for result in results:
-    print(f"Filename: {result['filename']}")
+    v1 = model.encode(question)
+
+    results = vector_index.search(v1, num_results=5)
+    for result in results:
+        print(f"Filename: {result['filename']}")
+    print("Done with vector results\n")
+    return results
+
+
+if __name__ == "__main__":
+    documents = download_documents()
+    # q1 = "What metric do we use to evaluate a search engine?"
+    q1 = "How do I store vectors in PostgreSQL?"
+    v_search(documents, q1)
