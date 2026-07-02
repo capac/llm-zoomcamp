30debc690d2a82bfbd6c5a4ea66d3bacedccfaa9
capac <capac@users.noreply.github.com>
2026-07-02 09:54:56 +0100

    Created 't_search' module
    Imported in 'hybrid_search' for homework #2 question #6

--- 2026/homework/02-vector-search/text_search.py
+++ 2026/homework/02-vector-search/text_search.py
@@ -1,16 +1,25 @@
 from ingest import download_documents
 from gitsource import chunk_documents
 from minsearch import Index
+from typing import List
 
-documents = download_documents()
-chunks = chunk_documents(documents, size=2000, step=1000)
 
-text_index = Index(text_fields=["content"])
-text_index.fit(chunks)
+def t_search(documents: List, question: str) -> List[str]:
+    chunks = chunk_documents(documents, size=2000, step=1000)
 
-print("Indexing completed.")
+    text_index = Index(text_fields=["content"])
+    text_index.fit(chunks)
 
-q1 = "How do I store vectors in PostgreSQL?"
-search_results = text_index.search(q1, num_results=5)
-for result in search_results:
-    print(f"Filename: {result['filename']}")
+    print("Indexing completed\n")
+
+    results = text_index.search(question, num_results=5)
+    for result in results:
+        print(f"Filename: {result['filename']}")
+    print("Done with text results\n")
+    return results
+
+
+if __name__ == "__main__":
+    documents = download_documents()
+    q1 = "How do I store vectors in PostgreSQL?"
+    t_search(documents, q1)
