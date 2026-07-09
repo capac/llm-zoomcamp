bea681e9454fc0759b6ef0477853881c374d7784
capac <capac@users.noreply.github.com>
2026-07-09 16:49:03 +0100

    Corrected solution for homework #2 question #3

--- 2026/homework/02-vector-search/chunking_and_search.py
+++ 2026/homework/02-vector-search/chunking_and_search.py
@@ -7,20 +7,20 @@ import numpy as np
 documents = download_documents()
 chunks = chunk_documents(documents, size=2000, step=1000)
 
+texts = [chunk["content"] for chunk in chunks]
 model = Embedder()
-
-chunk_dict = {}
-for chunk in chunks:
-    chunk_dict[chunk["filename"]] = model.encode(chunk["content"])
-
-X = np.array(list(chunk_dict.values()))
-y = np.array(list(chunk_dict.keys()))
+X = model.encode_batch(texts)
 
 q1 = "How does approximate nearest neighbor search work?"
 v1 = model.encode(q1)
 
 scores = X.dot(v1)
 
-top5_indexes = np.argsort(-scores)[:5]
-print(f"Scores of top 5 most similar chunks to q1:\n{scores[top5_indexes]}")
-print(f"Filenames for top 5 most similar chunks to q1:\n{y[top5_indexes]}")
+top5_idx = np.argsort(-scores)[:5]
+print("Scores of top 5 most similar chunks to q1:")
+for idx in top5_idx:
+    print(f"{np.round(scores[idx], 5)}")
+print(end="\n")
+print("Filenames for top 5 most similar chunks to q1:")
+for idx in top5_idx:
+    print(f"{chunks[idx]['filename']}")
